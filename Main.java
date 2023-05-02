a0ebf0665b1b969fe1af3f944979d6fc0ac16f7d
Sigmacci <mamchur.paviel@gmail.com>
2023-05-02 22:37:50 +0200

    Adjacency and graph matrix

--- Main.java
+++ Main.java
@@ -1,89 +1,152 @@
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Graph g = new Graph();
-        int[] arr = { 1, 2, 3, 4, 5 };
-        g.addVertex(1);
-        g.addVertex(2);
-        g.addVertex(3);
-        g.addVertex(4);
-        g.addVertex(5);
-        g.addEdge(1, 2);
-        g.addEdge(1, 3);
-        g.addEdge(1, 5);
-        g.addEdge(2, 3);
-        g.addEdge(2, 4);
-        g.addEdge(2, 5);
-        g.addEdge(3, 4);
-        g.addEdge(4, 5);
-        g.printAdjacencyMatrix();
+        Graph bg = new Graph();
+        bg.addVertex(1);
+        bg.addVertex(2);
+        bg.addVertex(3);
+        bg.addVertex(4);
+        bg.addVertex(5);
+        bg.addEdge(1, 2);
+        bg.addEdge(2, 5);
+        bg.addEdge(2, 4);
+        bg.addEdge(3, 1);
+        bg.addEdge(3, 2);
+        bg.addEdge(4, 3);
+        bg.addEdge(5, 4);
+        bg.addEdge(5, 1);
+        bg.getGraphMatrix();
     }
 }
 
 class Vertex {
     int key;
+    ArrayList<Vertex> in, out;
 
     public Vertex(int key) {
         this.key = key;
+        this.in = new ArrayList<>();
+        this.out = new ArrayList<>();
     }
 }
 
 class Graph {
-    private Map<Vertex, List<Vertex>> map;
+    ArrayList<Vertex> vList;
+    int adjacencyMatrix[][];
+    int graphMatrix[][];
 
     public Graph() {
-        map = new HashMap<>();
+        vList = new ArrayList<>();
     }
 
-    public void addVertex(int val) {
-        map.putIfAbsent(new Vertex(val), new ArrayList<>());
+    public void addVertex(int key) {
+        vList.add(new Vertex(key));
     }
 
-    public void addEdge(int first, int second) {
-        Vertex v1 = map.keySet().stream().filter(v -> v.key == first).findFirst().orElse(null);
-        Vertex v2 = map.keySet().stream().filter(v -> v.key == second).findFirst().orElse(null);
-        if (v1 != null && v2 != null) {
-            map.get(v1).add(v2);
-            map.get(v2).add(v1);
+    public void addEdge(int from, int to) {
+        Optional<Vertex> v1 = vList.stream().filter(v -> v.key == from).findFirst();
+        Optional<Vertex> v2 = vList.stream().filter(v -> v.key == to).findFirst();
+        if (v1.isPresent() && v2.isPresent()) {
+            v1.get().out.add(v2.get());
+            v2.get().in.add(v1.get());
+        } else {
+            System.out.println("Can't create edge. One of vertices doesn't exist");
         }
     }
 
-    public void printAdjacentVertices() {
-        for (Map.Entry<Vertex, List<Vertex>> entry : map.entrySet()) {
-            Vertex vertex = entry.getKey();
-            List<Vertex> adjacentVertices = entry.getValue();
-            System.out.print("Vertex " + vertex.key + " is adjacent to: ");
-            for (Vertex adjacentVertex : adjacentVertices) {
-                System.out.print(adjacentVertex.key + " ");
+    public void printList() {
+        for (Vertex v : vList) {
+            System.out.print("Vertex: " + v.key + " In: ");
+            if (v.in.isEmpty()) {
+                System.out.print("Empty ");
+            }
+            for (Vertex vIn : v.in) {
+                System.out.print(vIn.key + " ");
+            }
+            System.out.print("Out: ");
+            if (v.out.isEmpty()) {
+                System.out.print("Empty ");
+            }
+            for (Vertex vOut : v.out) {
+                System.out.print(vOut.key + " ");
             }
             System.out.println();
         }
     }
 
-    public void printAdjacencyMatrix() {
-        int n = map.keySet().size();
-        int m[][] = new int[n][n];
-        for (Map.Entry<Vertex, List<Vertex>> entry : map.entrySet()) {
-            Vertex vertex = entry.getKey();
-            int i = vertex.key - 1;
-            List<Vertex> adjacentVertices = entry.getValue();
-            for (Vertex adjacentVertex : adjacentVertices) {
-                int j = adjacentVertex.key - 1;
-                m[i][j] = m[j][i] = 1;
+    public void getAdjacencyMatrix() {
+        adjacencyMatrix = new int[vList.size()][vList.size()];
+        for (int i = 0; i < vList.size(); i++) {
+            for (int j = 0; j < vList.size(); j++) {
+                adjacencyMatrix[i][j] = 0;
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(m[i][j] + " ");
+        for (Vertex v : vList) {
+            if (!v.in.isEmpty())
+                for (Vertex vIn : v.in) {
+                    adjacencyMatrix[vList.indexOf(v)][vList.indexOf(vIn)] = -1;
+                }
+            if (!v.out.isEmpty())
+                for (Vertex vOut : v.out) {
+                    adjacencyMatrix[vList.indexOf(v)][vList.indexOf(vOut)] = 1;
+                }
+        }
+        for (int i = 0; i < vList.size(); i++) {
+            for (int j = 0; j < vList.size(); j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
             }
             System.out.println();
         }
     }
-    
+
+    public void getGraphMatrix() {
+        vList.sort(Comparator.comparing(v -> v.key));
+        for (var vertex : vList) {
+            vertex.in.sort(Comparator.comparing(v -> v.key));
+            vertex.out.sort(Comparator.comparing(v -> v.key));
+        }
+        graphMatrix = new int[vList.size()][vList.size() + 3];
+        for (int i = 0; i < vList.size(); i++) {
+            if (!vList.get(i).in.isEmpty() && !vList.get(i).out.isEmpty()) {
+                graphMatrix[i][vList.size()] = vList.get(i).out.stream().min(Comparator.comparing(v -> v.key))
+                        .get().key;
+                graphMatrix[i][vList.size() + 1] = vList.get(i).in.stream().min(Comparator.comparing(v -> v.key))
+                        .get().key;
+                int index = i;
+                graphMatrix[i][vList.size() + 2] = vList.stream()
+                        .filter(v -> !v.in.contains(vList.get(index)) && !v.out.contains(vList.get(index))).findFirst()
+                        .orElse(null).key;
+            }
+            for (int j = 0; j < vList.size(); j++) {
+                if (vList.get(i).out.contains(vList.get(j))) {
+                    graphMatrix[i][j] = vList.get(i).out.get(vList.get(i).out.size() - 1).key;
+                } else if (vList.get(i).in.contains(vList.get(j))) {
+                    graphMatrix[i][j] = vList.get(i).in.get(vList.get(i).in.size() - 1).key + vList.size();
+                } else {
+                    int last = 0;
+                    for (var v : vList) {
+                        if (!v.in.contains(vList.get(i)) && !v.out.contains(vList.get(i)))
+                            last = v.key;
+                    }
+                    int f = last;
+                    graphMatrix[i][j] = -vList.stream().filter(v -> v.key == f).findFirst().orElse(null).key;
+                }
+            }
+        }
+        for (int i = 0; i < vList.size(); i++) {
+            for (int j = 0; j < vList.size() + 3; j++) {
+                System.out.print(graphMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
